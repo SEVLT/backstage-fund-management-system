a1bc19ae16b298fe15eb5d011103e1e45c546883
SEVLT <sevlt@foxmail.com>
2020-09-25 21:48:20 +0800

    注册表单实现

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,18 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../views/Index.vue'
+import Register from '../views/Register'
+import NotFound from '../views/404'
 
 Vue.use(VueRouter)
 
 const routes = [
 	{
-    path: '/',
-    redirect:'/index'
+		path: '/',
+		redirect: '/index',
 	},
 	{
 		path: '/index',
 		name: 'index',
 		component: Index,
+	},
+	{
+		path: '/register',
+		name: 'register',
+		component: Register,
+	},
+	{
+		path: '*',
+		name: 'NotFound',
+		component: NotFound,
 	},
 ]
 
